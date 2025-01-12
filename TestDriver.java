919fa9f430d24c1f7ade694db166dad573a3d0d8
Clara Siefke <clarasiefkeprofessional@gmail.com>
2025-01-11 20:34:36 -0500

    Test toString() method of Point class.

--- TestDriver.java
+++ TestDriver.java
@@ -11,7 +11,33 @@ public class TestDriver
     */
     public static void main(String[] args)
         {
-        //Display title
+        //BEGIN TESTS
         System.out.println("\n***** PAT METHOD TESTS *****");
+
+        //POINT METHODS
+        System.out.println("\n**** Point Methods ****");
+        System.out.println("*** Testing toString ***");
+        System.out.println("Creating ten Point objects...");
+        Point point1 = new Point(0,0);
+        Point point2 = new Point(1,1);
+        Point point3 = new Point(5,7);
+        Point point4 = new Point(0.5,0.2);
+        Point point5 = new Point(-0.4, -0.7);
+        Point point6 = new Point(-0.6,0.3);
+        Point point7 = new Point(0,-1);
+        Point point8 = new Point(0.8,0.8);
+        Point point9 = new Point(-0.2,-0.2);
+        Point point10 = new Point(0.05,-0.004);
+        System.out.println("Point objects created.");
+        System.out.println("point1 (should be (0.0,0.0)): " + point1.toString());
+        System.out.println("point2 (should be (1.0,1.0)): " + point2.toString());
+        System.out.println("point3 (should be (5.0,7.0)): " + point3.toString());
+        System.out.println("point4 (should be (0.5,0.2)): " + point4.toString());
+        System.out.println("point5 (should be (-0.4,-0.7)): " + point5.toString());
+        System.out.println("point6 (should be (-0.6,0.3)): " + point6.toString());
+        System.out.println("point7 (should be (0.0,-1.0)): " + point7.toString());
+        System.out.println("point8 (should be (0.8,0.8)): " + point8.toString());
+        System.out.println("point9 (should be (-0.2,-0.2)): " + point9.toString());
+        System.out.println("point10 (should be (0.05,-0.004)): " + point10.toString());
         }
     }
